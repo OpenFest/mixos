d25851cb31f70ad4d37171bcac4dc3b3b085d22b
dexterlb <dexterlb@qtrp.org>
2025-05-29 16:43:20 +0300

    successfully build jack_mixer package

--- common/pkg-overrides.nix
+++ common/pkg-overrides.nix
@@ -6,14 +6,34 @@ final: prev: {
     pname = "jack_mixer";
     version = "19";
     src = prev.fetchFromGitHub {
-      owner = "jack-mixer";
+      # owner = "jack-mixer";
+      owner = "dexterlb";
       repo = "jack_mixer";
-      rev = "4feeafa496b166dd10a1c2a26b2afe9cb5c41d87";
-      hash = "sha256-ElpoDJ8DJI8bV3PEAPhtTxr2JgFcRBQIp1rxpVVSpqI=";
+      rev = "64a26f58e08dc6088561dd4b290ca7ca02d25bce";
+      hash = "sha256-9SzhpYrxGUK1L2U3aCuzAnEKLzSXTF+3/yJRrdnz7/g=";
     };
+    preConfigure = ''
+      mesonFlagsArray+=(-Draysessiondir=$prefix/etc/xdg/raysession/client_templates/35_jackmixer)
+    '';
     nativeBuildInputs = [
       prev.meson
     ];
+    preInstall = ''
+      patchShebangs --build /build/source/meson_postinstall.py
+
+      # no need to update gtk icon cache during packaging...
+      sed -i 's/gtk-update-icon-cache/true/g' /build/source/meson_postinstall.py
+    '';
+    # installPhase = ''
+    #   stat /build/source/meson_postinstall.py
+    #   local flagsArray=()
+    #   echo 'test run'
+    #   /usr/bin/env
+    #   /build/source/meson_postinstall.py
+
+    #   concatTo flagsArray mesonInstallFlags mesonInstallFlagsArray
+    #   ${prev.strace}/bin/strace -f -- meson install --no-rebuild "''${flagsArray[@]}"
+    # '';
     buildInputs = [
       prev.glib
       prev.pkg-config
