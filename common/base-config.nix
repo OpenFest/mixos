c652014c5593a5c937efac1b019db28298c83f8d
gotha <h.georgiev@hotmail.com>
2025-09-07 09:08:32 +0300

    enable grml for zsh

--- common/base-config.nix
+++ common/base-config.nix
@@ -114,6 +114,13 @@ in {
   programs.sway.enable = true;
 
   programs.zsh.enable = true;
+  programs.zsh.interactiveShellInit = ''
+    source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+
+    # Make user colour green in prompt instead of default blue
+    zstyle ':prompt:grml:left:items:user' pre '%F{green}%B'
+  '';
+  programs.zsh.promptInit = ""; # otherwise it'll override the grml prompt
 
   users.users.human.openssh.authorizedKeys.keys = ring-bearers;
 }
