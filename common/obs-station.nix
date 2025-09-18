0d4b63bcf253cc7fd49cf45c930de9855522325c
dexterlb <dexterlb@qtrp.org>
2025-09-18 13:32:15 +0300

    update obs-station config

--- common/obs-station.nix
+++ common/obs-station.nix
@@ -6,56 +6,9 @@
     pkgs.obs-studio
     pkgs.obs-studio-plugins.advanced-scene-switcher
     pkgs.guvcview
-
-    # gui shit
-    pkgs.firefox
-    pkgs.tigervnc
-    pkgs.alacritty
-    pkgs.fuzzel
   ];
 
-  services.displayManager = {
-    defaultSession = "sway";
-    autoLogin.enable = true;
-    autoLogin.user = "human";
-    sddm = {
-      enable = true;
-      wayland = { enable = true; };
-    };
-  };
-
-  programs.sway =
-    let swayData = ((import ../configs/sway) { inherit pkgs lib; });
-    in {
-      enable = true;
-      extraOptions = [
-        "--unsupported-gpu" # my next GPU will not be nvidia
-        "--config"
-        "${swayData.packages.sway-user-data}/config"
-      ];
-      extraSessionCommands = ''
-        {
-          echo "sourcing ${swayData.packages.sway-session-script}/bin/sway-session-script"
-          source "${swayData.packages.sway-session-script}/bin/sway-session-script"
-        } &> /tmp/session_log
-      '';
-    };
-
-  xdg.portal = {
-    enable = true;
-    # config = {
-    #   common = {
-    #     default = [
-    #       "gtk"
-    #     ];
-    #   };
-    # };
-    wlr = { enable = true; };
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    xdgOpenUsePortal = true;
-  };
-
-  services.xserver = { enable = false; };
+  home-manager.users.human.imports = [ ./gui-sway.nix ];
 
   services.pipewire.extraConfig.pipewire = {
     "55-obs-monitor-sink" = {
--- templates/obs-station-nvidia/default.nix
+++ templates/obs-station-nvidia/default.nix
@@ -3,7 +3,8 @@
     ../../common/raw-efi.nix
     ../../common/base-config.nix
     ../../common/audio-config.nix
-    #../../common/nvidia.nix
+    # ../../common/nvidia.nix
     ../../common/grow-root.nix
+    ../../common/obs-station.nix
   ];
 }
