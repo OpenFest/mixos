2bd14990e5314457d9e58277e5faeb0a8beea92b
dexterlb <dexterlb@qtrp.org>
2025-09-19 15:24:09 +0300

    properly loop over all systems we have machines for

--- flake.nix
+++ flake.nix
@@ -23,10 +23,8 @@
       # get all templates, and from there get all hosts (≥1 hosts per template, usually 1)
       templateNames = dirsInDir templatesDir;
       hostList = builtins.concatLists (map templateHosts templateNames);
-      hosts = builtins.listToAttrs (map (host: {
-        name = host.hostname;
-        value = host;
-      }) hostList);
+      hosts = mkHostMap hostList;
+      hostsBySystem = system: mkHostMap (builtins.filter (host: host.system == system) hostList);
 
       # build a nixos configuration for the given host
       mkNixos = host: lib.nixosSystem (nixosSystemArgs host);
@@ -63,7 +61,8 @@
 
       # the usual shit
       lib = nixpkgs.lib;
-      forAllSystems = lib.genAttrs [ "aarch64-darwin" "x86_64-linux" ];
+      allSystems = lib.lists.unique (map (host: host.system) hostList);
+      forAllSystems = lib.genAttrs allSystems;
 
       # nixpkgs overlays (used for custom package overrides)
       overlays = map (src: import "${overlaysDir}/${src}") overlaySources;
@@ -83,6 +82,10 @@
         in map (data: data // { templateName = name; }) hostsData;
 
       # util functions
+      mkHostMap = hostList: builtins.listToAttrs (map (host: {
+        name = host.hostname;
+        value = host;
+      }) hostList);
       mapValues = f: attrset: builtins.mapAttrs (_: x: f x) attrset;
       mapValuesIf = f: attrset:
         lib.filterAttrs (_: x: x != null) (mapValues f attrset);
@@ -106,10 +109,10 @@
 
       nixosConfigurations = mapValues mkNixos hosts;
 
-      # e.g. self.packages.x86_64-linux.hala, self.packages.x86_64-linux.zver, ...
+      # expose disk images as packages
       packages = forAllSystems
-        (_: # FIXME: should this be the target system? I think we don't care
-          mapValuesIf mkImage hosts);
+        (system:
+          mapValuesIf mkImage (hostsBySystem system));
 
       # deploy-rs nodes
       deploy = {
@@ -118,8 +121,8 @@
       };
 
       # deploy-rs checks
-      checks = builtins.mapAttrs
-        (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+      checks = forAllSystems
+        (system: deploy-rs.lib.${system}.deployChecks self.deploy);
 
       # tiny helper so you can run `nix run .#deploy -- .#hala`
       apps = forAllSystems (system: {
