477656bd4e7682f994cfee4df040bc5b546991a0
dexterlb <dexterlb@qtrp.org>
2025-09-20 18:45:22 +0300

    remove fixme

--- common/networking-dhcp.nix
+++ common/networking-dhcp.nix
@@ -1,10 +1,5 @@
 { lib, hostname, ... }: {
   networking = rec {
-    # FIXME: decide if passing the hostname as a module argument is a good idea
-    # or should the flake just insert it as `networking.hostName` and be done with it?
-    # upside of the current approach is that it is flexible
-    # downside is that you have to write this in every single hosts config, and
-    # who would want to set the hostname to anything other than the host hostname anyway?
     hostName = hostname;
     domain = "video.fosdem.org";
     dhcpcd.enable = true;
