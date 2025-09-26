42d2e27456f82dd107b6325c60b3c3a3aebeee61
Albert Stefanov <aastefanov@outlook.com>
2025-09-26 07:08:51 +0300

    Make OBS controllable from external keyboards
    Closes #20

--- common/workstation-obs-and-external-audio-mixer.nix
+++ common/workstation-obs-and-external-audio-mixer.nix
@@ -1,5 +1,5 @@
 { inputs, lib, pkgs, ... }: {
-  imports = [ ./base-config.nix ./audio-config.nix ./obs ./qpwgraph ];
+  imports = [ ./base-config.nix ./audio-config.nix ./obs ./qpwgraph ./shanokeys ];
 
   mixos.qpwgraph.patchbay = "obs-audio";
 }
