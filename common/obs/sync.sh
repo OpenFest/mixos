88f1b6980e8bca9562d01dc63f5db5ab490f61eb
dexterlb <dexterlb@qtrp.org>
2025-10-04 11:40:07 +0300

    sync obs config

--- common/obs/sync.sh
+++ common/obs/sync.sh
@@ -4,20 +4,28 @@ set -euo pipefail
 cdir="$(dirname "$(readlink -f "${0}")")"
 
 function usage {
-    echo "usage: ${1} (from-my-computer|to-my-computer)"
+    echo "usage: ${1} (from-my-computer|to-my-computer|from-host <hostname>)"
     echo "synchronizes the obs config dir between this repo and the home directory of the user that runs the script"
     exit 1
 }
 
-function sync_from_home {
+function sync_from_dir {
     rsync -rva --delete-after --delete-excluded \
         --exclude=/logs \
         --exclude=/profiler_data \
         --exclude=/plugin_config/obs-browser \
         --exclude='*.bak' \
         --exclude=".version" \
-        "${HOME}"/.config/obs-studio/ \
+        "${1}" \
         "${cdir}"/obs-studio/
+}
+
+function sync_from_home {
+    sync_from_dir "${HOME}/.config/obs-studio/"
+}
+
+function sync_from_remote {
+    sync_from_dir "${1}:.config/obs-studio/"
 }
 
 function sync_to_home {
@@ -26,15 +34,27 @@ function sync_to_home {
         "${HOME}"/.config/obs-studio/
 }
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -eq 0 ]]; then
     usage "${0}"
 fi
 
 case "${1}" in
+    from-host)
+        if [[ $# -ne 2 ]]; then
+            usage "${0}"
+        fi
+        sync_from_remote "${2}"
+        ;;
     from-my-computer)
+        if [[ $# -ne 1 ]]; then
+            usage "${0}"
+        fi
         sync_from_home
         ;;
     to-my-computer)
+        if [[ $# -ne 1 ]]; then
+            usage "${0}"
+        fi
         sync_to_home
         ;;
     *)
