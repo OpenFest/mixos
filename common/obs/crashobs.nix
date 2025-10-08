25556b5c5e2bd539177e6e186184b8321420f792
dexterlb <dexterlb@qtrp.org>
2025-10-08 17:23:33 +0300

    cut often

--- common/obs/crashobs.nix
+++ common/obs/crashobs.nix
@@ -1,20 +1,16 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
+{ pkgs, config, lib, ... }:
 let
   keyboards = config.mixos.macro-keyboards;
-interval = "0.5";
+  interval = "0.5";
 
-scriptbody =lib.concatStrings(
-        lib.mapAttrsToList (keyboard: props:
-lib.concatStrings(lib.mapAttrsToList (key: cmd: ''
-echo 'sending ${cmd.endpoint}: ${builtins.toJSON cmd.params} to obs'
-${pkgs.shanokeys}/bin/shanokeys ${keyboard} ${key} >/dev/null || crashed
-sleep ${interval} 
-'') props.mappings)) keyboards);
+  scriptbody = lib.concatStrings (lib.mapAttrsToList (keyboard: props:
+    lib.concatStrings (lib.mapAttrsToList (key: cmd: ''
+      echo 'sending ${cmd.endpoint}: ${builtins.toJSON cmd.params} to obs'
+      ${pkgs.shanokeys}/bin/shanokeys ${keyboard} ${key} >/dev/null || crashed
+      sleep 0.1
+      ${pkgs.shanokeys}/bin/shanokeys ${keyboard} kpenter >/dev/null || crashed
+      sleep ${interval}
+    '') props.mappings)) keyboards);
 
   crashobs = pkgs.writeShellApplication {
     name = "crashobs";
@@ -22,18 +18,14 @@ sleep ${interval}
 
     text = ''
 
-crashed() {
-    echo "obs crashed"
-    exit 127
-}
+      crashed() {
+          echo "obs crashed"
+          exit 127
+      }
 
-while true; do
-${scriptbody}
-done
-'';
-}; in
-{
-  config = {
-    users.users.human.packages = [ crashobs ];
+      while true; do
+      ${scriptbody}
+      done
+    '';
   };
-}
+in { config = { users.users.human.packages = [ crashobs ]; }; }
